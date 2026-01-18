846ed75f562735617bb504c4547e7eda1c5b404e
nadine-elsayed <dodynadeen0@gmail.com>
2026-01-18 19:47:38 +0200

    Complete security pipeline with HTML reports

--- terraform/monitoring.tf
+++ terraform/monitoring.tf
@@ -7,7 +7,7 @@ resource "aws_sns_topic" "proshop_alerts" {
 resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
   topic_arn = aws_sns_topic.proshop_alerts.arn
   protocol  = "email"
-  endpoint  = "your-email@example.com" # <--- UPDATE THIS
+  endpoint  = "nadine.elsayed03@gmail.com" 
 }
 
 # CloudWatch Alarm for Cluster Health (Node CPU)
